8ed8db74375ae3117312a4ddba752ddd00391f75
enypy <g404ebus@gmail.com>
2024-10-29 21:23:03 +0100

    symfony bump 6.3->7.1, symfony-vercel-bundle add

--- api/index.php
+++ api/index.php
@@ -1,3 +1,9 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+use App\Kernel;
+
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
